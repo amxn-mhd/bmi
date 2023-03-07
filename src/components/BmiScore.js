6cb8b3f1b5f7cb31d3e55ff84479f3549befa3b8
amxn-mhd <amxnmhd@gmail.com>
2023-03-07 12:25:18 +0530

    last

---
+++ src/components/BmiScore.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function BmiScore({ BmiNo, BmiName , changeWeight}) {
+  return (
+    <div className="text-center shadow rounded p-4">
+      <div>Your BMI Score</div>
+      <div className="row justify-content-md-center">
+        <div className="p-3 my-2 alert fs-1 alert-primary col-sm-4">{BmiNo}</div>
+      </div>
+      <div className="fs-3 fw-bold text-primary">{BmiName}</div>
+      {changeWeight.type==="positive"&&(
+        <div className="fs-4">"You need to lose <span className="fw-bold">{changeWeight.wight} kg"</span></div>
+      )}
+      {changeWeight.type==="negative"&&(
+        <div className="fs-4">"You need to gain <span className="fw-bold">{changeWeight.wight} kg"</span></div>
+      )}
+      {changeWeight.type==="normal"&&(
+        <div className="fs-4">"Your weight is Normal,Good for you"</div>
+      )}
+    </div>
+  );
+}
+
+export default BmiScore;
